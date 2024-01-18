9f244b3b8419603d0ad8cbfbefa82c7a5a5a2b96
Alla Kokhaniuk <121404468+Alla-Ko@users.noreply.github.com>
2024-01-18 19:36:21 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApp_ProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(57)))), ((int)(((byte)(112)))));
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(711, 326);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(57)))), ((int)(((byte)(112)))));
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.progressBar1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(711, 50);
+            this.panel2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(219)))), ((int)(((byte)(227)))));
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(57)))), ((int)(((byte)(112)))));
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Завантажити";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(62)))), ((int)(((byte)(35)))));
+            this.progressBar1.Location = new System.Drawing.Point(162, 22);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(549, 28);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(219)))), ((int)(((byte)(227)))));
+            this.label1.Location = new System.Drawing.Point(162, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Процес завантаження...";
+            this.label1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(219)))), ((int)(((byte)(227)))));
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(57)))), ((int)(((byte)(112)))));
+            this.textBox1.Location = new System.Drawing.Point(0, 50);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(711, 276);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 326);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(419, 184);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Завантаження файлу";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Reflection.Emit;
+
+namespace WindowsFormsApp_ProgressBar
+{
+    public partial class Form1 : Form
+    {
+        
+        private StringBuilder resultText;
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBackgroundWorker();
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            resultText = new StringBuilder();
+
+        }
+        
+        private void InitializeBackgroundWorker()
+        {
+            backgroundWorker1 = new BackgroundWorker();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.ProgressChanged += BackgroundWorker_ProgressChanged;
+            backgroundWorker1.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker1.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+        }
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string filename = e.Argument.ToString();
+            Encoding encoding = Encoding.GetEncoding("UTF-8");
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filename, encoding))
+                {
+                    long fileSize = new FileInfo(filename).Length;
+                    long bytesRead = 0;
+
+                    char[] buffer = new char[8192];
+                    int charsRead;
+                    
+
+                    while ((charsRead = reader.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        bytesRead += charsRead;
+
+                        // Додаємо текст до результату
+                        resultText.Append(new string(buffer, 0, charsRead));
+                        
+                        // Оновлюємо прогрес
+                        int progressPercentage = (int)((double)bytesRead / fileSize * 100);
+                        backgroundWorker1.ReportProgress(progressPercentage);
+                    }
+                    // Передаємо усі абзаци тексту як результат
+                    
+                    e.Result = resultText.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка читання файлу: " + ex.Message);
+            }
+        }
+        private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            //textBox1.Text = resultText.ToString();
+            
+        }
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+
+                textBox1.Text = resultText.ToString();
+                progressBar1.Value = 100;
+                MessageBox.Show("Файл завантажено!");
+                label1.Visible = false;
+                progressBar1.Visible=false;
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                progressBar1.ForeColor = Color.Red;
+            }
+            else
+            {
+                MessageBox.Show("Помилка: " + e.Error.Message);
+            }
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            progressBar1.Visible = false;
+            //відкрити з файла
+            textBox1.Text = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            //Encoding encoding = Encoding.GetEncoding("UTF-8");
+            // читаем файл в строку
+            progressBar1.Visible = true;
+            label1.Visible = true;
+            // Очищуємо StringBuilder перед кожним відкриттям файлу
+            resultText.Clear();
+            // Запускаємо BackgroundWorker для читання файлу
+            backgroundWorker1.RunWorkerAsync(filename);
+
+        }
+    }
+}
